bc6ac96837b6eec4c2a718511bf616fb3fd58845
Agustin K-ballo Berge <k@fusionfenix.com>
2020-04-04 10:55:24 -0300

    Conan recipe Catch2/2.11.1@bitwyre/stable

--- conanfile.py
+++ conanfile.py
@@ -4,6 +4,7 @@ from conans import ConanFile, CMake
 
 class CatchConan(ConanFile):
     name = "Catch2"
+    version = "2.11.1"
     description = "A modern, C++-native, header-only, framework for unit-tests, TDD and BDD"
     topics = ("conan", "catch2", "header-only", "unit-test", "tdd", "bdd")
     url = "https://github.com/catchorg/Catch2"
@@ -11,14 +12,20 @@ class CatchConan(ConanFile):
     license = "BSL-1.0"
     exports = "LICENSE.txt"
     exports_sources = ("single_include/*", "CMakeLists.txt", "CMake/*", "contrib/*")
+    no_copy_source = True
     generators = "cmake"
 
     def package(self):
         cmake = CMake(self)
-        cmake.definitions["BUILD_TESTING"] = "OFF"
-        cmake.definitions["CATCH_INSTALL_DOCS"] = "OFF"
-        cmake.definitions["CATCH_INSTALL_HELPERS"] = "ON"
-        cmake.configure(build_folder='build')
+        cmake.definitions["BUILD_TESTING"] = False
+        cmake.definitions["CATCH_BUILD_TESTING"] = False
+        cmake.definitions["CATCH_BUILD_EXAMPLES"] = False
+        cmake.definitions["CATCH_BUILD_EXTRA_TESTS"] = False
+        cmake.definitions["CATCH_ENABLE_COVERAGE"] = False
+        cmake.definitions["CATCH_ENABLE_WERROR"] = False
+        cmake.definitions["CATCH_INSTALL_DOCS"] = False
+        cmake.definitions["CATCH_INSTALL_HELPERS"] = True
+        cmake.configure()
         cmake.install()
 
         self.copy(pattern="LICENSE.txt", dst="licenses")
